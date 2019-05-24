b0bfe5ec0c35dd1b7591aaf6895b12c15507a8be
Sergey Maslennikov <maslennikovserge@yandex.ru>
2019-05-24 19:06:24 +0300

    csharp

---
+++ CSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ExtensionMethod.Test();
+        }
+    }
+}
