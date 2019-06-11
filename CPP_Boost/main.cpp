83ae3c735cd1861ee0b844ed91127b2f9a3422ee
Sergey Maslennikov <maslennikovserge@yandex.ru>
2019-06-11 14:26:05 +0300

    cpp_boost

--- CPP_Boost/main.cpp
+++ CPP_Boost/main.cpp
@@ -44,10 +44,13 @@ awaitable<void> echo(tcp::socket socket)
 awaitable<void> listener()
 {
 	auto executor = co_await this_coro::executor;
+
 	tcp::acceptor acceptor(executor, { tcp::v4(), 55555 });
+
 	for (;;)
 	{
 		tcp::socket socket = co_await acceptor.async_accept(use_awaitable);
+
 		co_spawn(executor,
 			[socket = std::move(socket)]() mutable
 		{
@@ -64,6 +67,7 @@ int main()
 		boost::asio::io_context io_context(1);
 
 		boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+
 		signals.async_wait([&](auto, auto) { io_context.stop(); });
 
 		co_spawn(io_context, listener, detached);
