28f075ab62f0e5deaf8c4e027930dfd74ef286d5
Sergey Maslennikov <maslennikovserge@yandex.ru>
2019-06-18 23:30:03 +0300

    recurrens

--- CPP_DEMO_2/main.cpp
+++ CPP_DEMO_2/main.cpp
@@ -1,10 +1,27 @@
+#include <iostream>
+
 void Packet_1();
 void Packet_2();
+
+int Reccurrens(int value, int figure, int count)
+{
+	if (--count > 0)
+	{
+		return Reccurrens(value + figure, figure, count);
+	}
+
+	return value;
+}
 
 int main()
 {
 	//Packet_1();
-	Packet_2();
+	//Packet_2();
+
+	for (int i = 0; i < 9; ++i)
+	{
+		std::cout << Reccurrens(i, i, i) << '\n';
+	}
 
 	return 0;
 }
