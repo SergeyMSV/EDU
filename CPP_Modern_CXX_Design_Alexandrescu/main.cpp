7231758e308d675079e767cc40825f5e4caf5a88
Sergey.Maslennikov <Sergey.Maslennikov@winpharma.com>
2019-12-06 11:46:54 +0300

    CPP_Modern_CXX_Design_Alexandrescu

--- CPP_Modern_CXX_Design_Alexandrescu/main.cpp
+++ CPP_Modern_CXX_Design_Alexandrescu/main.cpp
@@ -1,10 +1,17 @@
-namespace TL
-{
-	void UnitTest();
-}
+namespace Cloneable { void UnitTest(); }
+namespace Cloneable2 { void UnitTest(); }
+namespace Convert { void UnitTest(); }
+namespace Int2Type { void UnitTest(); }
+namespace Type2Type { void UnitTest(); }
+namespace TL { void UnitTest(); }
 
 int main()
 {
+	Cloneable::UnitTest();
+	Cloneable2::UnitTest();
+	Convert::UnitTest();
+	Int2Type::UnitTest();
+	Type2Type::UnitTest();
 	TL::UnitTest();
 
 	return 0;
