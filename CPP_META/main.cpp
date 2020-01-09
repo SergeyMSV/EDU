783ab5d9ac1d747c5d3eb3c1a289b87f7f1b6b05
Sergey.Maslennikov <Sergey.Maslennikov@winpharma.com>
2020-01-09 14:59:03 +0300

    cpp_meta

--- CPP_META/main.cpp
+++ CPP_META/main.cpp
@@ -1,8 +1,10 @@
+void UnitTest_Class();//preferable
 int main_meta_difference_type();
 int main_meta_has_foo();
 
 int main()
 {
+	UnitTest_Class();
 	main_meta_difference_type();
 	main_meta_has_foo();
 
